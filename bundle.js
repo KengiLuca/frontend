b7905dca82d6e9db1033dd2c4f166558615a9746
KengiLuca <kengiluca@gmail.com>
2023-10-30 14:09:25 +0000

    DOM

--- bundle.js
+++ bundle.js
@@ -2,14 +2,29 @@
 const fruitForm = document.querySelector("#inputSection form")
 console.log(fruitForm)
 
-const david = require("./script2")
+fruitForm.addEventListener("submit", extractFruit)
 
-console.log(david)
+function extractFruit(e){
+    e.preventDefault() // Prevent the page from refreshing instantly
+    console.log(e) // console logging the form
+    addFruit(e.target[0].value) // Adding a list in the fruit section with the value of the fruit input name
+    e.target[0].value = "" // Resetting the form so can add more fruits
+}
 
+const fruitList = document.querySelector("#fruitSection ul")
+function addFruit(fruit){
+    if (!fruit){
+        console.log("Invalid fruit")
+    } else {
+    const li = document.createElement("li")
+    li.addEventListener("click", removeFruit, {once:true})
+    li.textContent = fruit
+    fruitList.appendChild(li)
+    }
+}
+function removeFruit(e){
+    e.target.remove()
+}
 
-},{"./script2":2}],2:[function(require,module,exports){
-const x = 'david'
-
-module.exports = x
 
 },{}]},{},[1]);
